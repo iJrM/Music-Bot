5dad967bf096488cd5d0530577e00769e5a59437
iJrM <44652718+iJrM@users.noreply.github.com>
2019-02-02 23:50:49 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
-exports.TOKEN = '';
+exports.TOKEN = 'NTQxMzU2MjQxNDc4NDgzOTg5.DzeRcQ.V9Q0fF_VEWteOFVIRw3E0azlF7Y';
 
-exports.PREFIX = 'm!';
+exports.PREFIX = 'M';
 
-exports.GOOGLE_API_KEY = '';
+exports.GOOGLE_API_KEY = '912';
